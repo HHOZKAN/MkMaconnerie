38b5ef80af05d74d5f69ecb8918c3ead8380efe5
HHOZKAN <ozkn.haci@gmail.com>
2025-07-11 16:38:41 +0200

    enddd

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -63,7 +63,7 @@ export const Header = () => {
             <div className="hidden md:flex items-center space-x-3">
               <a href="tel:+33123456789" className="flex items-center space-x-2 text-amber-700">
                 <Phone className="w-4 h-4" />
-                <span className="font-medium">06 12 34 56 78</span>
+                <span className="font-medium">06 99 91 86 88</span>
               </a>
               <Button 
                 onClick={() => setIsQuoteFormOpen(true)}
